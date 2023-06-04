12f9c98680c22f1dcdb915a0d1be279ad07529a8
rliu6915 <rliu6915@connect.hku.hk>
2023-06-04 22:37:06 +0800

    make change for attemptLogin() method

--- org/DataManager.java
+++ org/DataManager.java
@@ -39,7 +39,7 @@ public class DataManager {
 				JSONObject data = (JSONObject)json.get("data");
 				String fundId = (String)data.get("_id");
 				String name = (String)data.get("name");
-				String description = (String)data.get("descrption");
+				String description = (String)data.get("description");
 				Organization org = new Organization(fundId, name, description);
 
 				JSONArray funds = (JSONArray)data.get("funds");
@@ -76,7 +76,7 @@ public class DataManager {
 			else return null;
 		}
 		catch (Exception e) {
-			e.printStackTrace();
+			// e.printStackTrace();
 			return null;
 		}
 	}
