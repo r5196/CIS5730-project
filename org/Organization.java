88544bc9d06e44abb73316e62c90b8d6eded9598
Yao Jiang <92890767+cleoyaojiang@users.noreply.github.com>
2023-06-25 03:28:37 -0400

    Update Organization.java with setName and setDescription

--- org/Organization.java
+++ org/Organization.java
@@ -35,6 +35,13 @@ public class Organization {
 	public void addFund(Fund fund) {
 		funds.add(fund);
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
